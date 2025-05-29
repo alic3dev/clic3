145f6c43bbe170db3296eae6d83f26a8739d6127
alic3 <alic3dev@gmail.com>
2025-05-28 22:02:15 -0700

    `clic3_bytes`:add

--- include/clic3.h
+++ include/clic3.h
@@ -1,6 +1,7 @@
 #ifndef __clic3_h
 #define __clic3_h
 
+#include <clic3_bytes.h>
 #include <clic3_char_arrays.h>
 #include <clic3_char_values.h>
 #include <clic3_sort.h>
---
+++ include/clic3_bytes.h
@@ -0,0 +1,10 @@
+#ifndef __clic3_bytes_h
+#define __clic3_bytes_h
+
+void clic3_bytes_copy(
+  void*,
+  void*,
+  unsigned int
+);
+
+#endif
