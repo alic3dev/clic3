dca7a3d758f892ae4af65814f1bab6a9cf964fdc
alic3 <alic3dev@gmail.com>
2025-11-18 01:34:29 -0800

    `clic3_char_arrays`:add->{`clic3_char_array_split_on_char`};

--- sources/clic3_char_arrays.c
+++ sources/clic3_char_arrays.c
@@ -494,3 +494,137 @@ char* clic3_char_arrays_concatenate(
 
   return char_array_destination;
 }
+
+
+char** clic3_char_array_split_on_char(
+  char* char_array,
+  char deliminator
+) {
+  static char** split_char_arrays;
+  
+  split_char_arrays = malloc(
+    sizeof(char*) *
+    1
+  );
+
+  split_char_arrays[0] = 0;
+
+  unsigned long int index_previous_split_char_buffer = 0;
+  unsigned long int index_char_array = 0;
+
+  while (
+    char_array[
+      index_char_array
+    ] != '\0'
+  ) {
+    if (
+      char_array[
+        index_char_array
+      ] == deliminator ||
+      char_array[
+        index_char_array +
+        1
+      ] == '\0'
+    ) {
+      split_char_arrays[0] = (
+        split_char_arrays[0] +
+        1
+      );
+
+      split_char_arrays = realloc(
+        split_char_arrays,
+        sizeof(char*) * (
+          (unsigned long int) (
+            split_char_arrays[0]
+          ) +
+          1
+        )
+      );
+
+      unsigned long int length_split_char_array = (
+        index_char_array -
+        index_previous_split_char_buffer + (
+          char_array[
+            index_char_array +
+            1
+          ] == '\0' &&
+          char_array[
+            index_char_array
+          ] != deliminator
+          ? 1
+          : 0
+        )
+      );
+
+      split_char_arrays[
+        (unsigned long int) split_char_arrays[0]
+      ] = malloc(
+        sizeof(char) *
+        length_split_char_array +
+        1
+      );
+
+      clic3_bytes_copy(
+        split_char_arrays[
+          (unsigned long int) split_char_arrays[0]
+        ], (
+          char_array +
+          index_previous_split_char_buffer
+        ),
+        length_split_char_array
+      );
+
+      split_char_arrays[
+        (unsigned long int) split_char_arrays[0]
+      ][
+        length_split_char_array
+      ] = '\0';
+
+      index_previous_split_char_buffer = (
+        index_char_array +
+        1
+      );
+    }
+
+    index_char_array = (
+      index_char_array +
+      1
+    );
+  }
+
+  if (
+    index_char_array > 0 &&
+    char_array[
+      index_char_array - 1
+    ] == deliminator
+  ) {
+    split_char_arrays[0] = (
+      split_char_arrays[0] +
+      1
+    );
+
+    split_char_arrays = realloc(
+      split_char_arrays,
+      sizeof(char*) * (
+        (unsigned long int) (
+          split_char_arrays[0]
+        ) +
+        1
+      )
+    );
+
+    split_char_arrays[
+      (unsigned long int) split_char_arrays[0]
+    ] = malloc(
+      sizeof(char)
+    );
+
+    split_char_arrays[
+      (unsigned long int) split_char_arrays[0]
+    ][0] = '\0';
+  }
+
+  return (
+    split_char_arrays
+  );
+}
