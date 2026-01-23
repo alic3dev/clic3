b8f96deae723e879cb81ab7ec95804e99ff3095c
alic3 <alic3dev@gmail.com>
2026-01-22 21:35:02 -0800

    `clic3_numbers`:add

---
+++ include/clic3_numbers.h
@@ -0,0 +1,18 @@
+#ifndef __clic3_numbers_h
+#define __clic3_numbers_h
+
+#define clic3_numbers_definition_length 10
+
+#define clic3_numbers_definition \
+  '0',\
+  '1',\
+  '2',\
+  '3',\
+  '4',\
+  '5',\
+  '6',\
+  '7',\
+  '8',\
+  '9'
+
+#endif
