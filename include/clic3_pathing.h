8af6d4cb07c955724e6e8337a93bce8b5cbd9d72
alic3dev <alice@alic3.dev>
2026-06-01 22:53:14 -0700

    `clic3_pathing`:initial

---
+++ include/clic3_pathing.h
@@ -0,0 +1,8 @@
+#ifndef __clic3_clic3_pathing_h
+#define __clic3_clic3_pathing_h
+
+char* clic3_pathing_combine(
+  ...
+);
+
+#endif
---
+++ sources/clic3_pathing.c
@@ -0,0 +1,17 @@
+#include <clic3_pathing.h>
+
+#include <clic3_memory.h>
+
+char* clic3_pathing_combine(
+) {
+  char* pathing = (
+    clic3_memory_allocate_raw(
+      0x00
+    )
+  );
+  
+  return (
+    pathing
+  );
+}
+
